abe1d9168f4b3e8f8ef534e58a0fb1f004ac6c52
MadhuniT <2019cs168@stu.ucsc.lk>
2022-08-21 22:47:00 +0530

    this commit for user registration backend 1

--- query/student.js
+++ query/student.js
@@ -1,2 +1,4 @@
 exports.CHECK_STUDENT_EMAIL = "SELECT * FROM users WHERE email = ?";
 exports.REGISTER_STUDENT = " INSERT INTO users  VALUES( Null, ?)" ;
+exports.REGISTER_CLIENT = " INSERT INTO client VALUES(Null,Null,Null,?,?,?,?)" ;
+exports.REGISTER_USERS = " INSERT INTO users VALUES(Null,'1',?,?,?,?)" ;
