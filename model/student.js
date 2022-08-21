ee2ed12ac684fa08f3b885ac016f85812fad6b47
MadhuniT <2019cs168@stu.ucsc.lk>
2022-08-21 23:20:48 +0530

    Revert "this commit for user registration backend 1"
    This reverts commit abe1d9168f4b3e8f8ef534e58a0fb1f004ac6c52.

--- model/student.js
+++ model/student.js
@@ -1,14 +1,15 @@
 const Joi = require('@hapi/joi');
 
- exports.STUDENT_MODEL = Joi.object({
-     username: Joi.string().min(3).max(100).required(),
-     email: Joi.string().email().required(),
-     contactNumber: Joi.number().required(),
-     addressOne: Joi.string().min(3).max(100).required(),
-     addressTwo: Joi.string().min(3).max(100).required(),
-     nic: Joi.number().required(),
-     password: Joi.string().min(6).required()
+exports.STUDENT_MODEL = Joi.object({
+    s_id: Joi.string(),
+    // age: Joi.number().required(),
+    // rank: Joi.number().integer().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
+    name: Joi.string().min(3).max(100).required(),
+    type: Joi.number().required(),
 })
+
 exports.STUDENT_LOGIN_MODEL = Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().min(6).required()
--- query/student.js
+++ query/student.js
@@ -1,4 +1,2 @@
 exports.CHECK_STUDENT_EMAIL = "SELECT * FROM users WHERE email = ?";
 exports.REGISTER_STUDENT = " INSERT INTO users  VALUES( Null, ?)" ;
-exports.REGISTER_CLIENT = " INSERT INTO client VALUES(Null,Null,Null,?,?,?,?)" ;
-exports.REGISTER_USERS = " INSERT INTO users VALUES(Null,'1',?,?,?,?)" ;
