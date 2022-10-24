89ad473fdb9f33c98479825119d756d79c45f0fb
kavira-skbs <kaviraskbs@gmail.com>
2022-10-24 17:52:20 +0530

    route od sp
    all at once

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const express = require('express');
 
 // require route
 
+const serviceprovider_route = require('./route/ServiceProvider/serviceprovider_routes'); // this is for serviceprovider route
 const student_route = require('./route/student_route'); // this for student route
 const admin_route = require('./route/Admin/admin_routs'); // this for admin route
 const student_todos_route = require('./route/student_todos');
 const al_route = require('./route/Astrologer/al_routs');
 const np_route = require('./route/Name-Provider/np_routs');
 const cl_route = require('./route/Client/cl_route');
-const chef_route = require('./route/Chef/chef_route');//this is for chef routs
 
 // require JWT auth Service
 const auth = require('./service/auth_service');
@@ -25,38 +25,38 @@ const cors = require('cors');
 
 
 // use cors middleware to enable CORS with various options
-app.use(cors({ origin: "*" }))
+app.use(cors({ origin: "*"}))
 
 //use express.json() middleware to parses incoming requests with JSON payloads
 //and is based on body-parser
 app.use(express.json());
 
+// if request come from "localhost:3000/serviceprovider", then it gose to serviceprovider_route
+app.use("/serviceprovider" , serviceprovider_route);
+
 // if request come from "localhost:3000/student", then it gose to student_route
-app.use("/systemlogin", student_route);
+app.use("/student" , student_route);
 
 // if request come from "localhost:3000/admin", then it goes to student_route
-app.use("/admin", admin_route);
+app.use("/admin" , admin_route);
 
 // if request come from "localhost:3000/todos", then it goes to student_todos_route
-app.use("/todos", student_todos_route);
+app.use("/todos" , auth,  student_todos_route);
 
 // if request come from "localhost:3000/Astrologer", then it goes to astrologer_route
-app.use("/al", al_route);
+app.use("/al" ,  al_route);
 
 // if request come from "localhost:3000/np/new_request", then it goes to name_provider_route
-app.use("/np", np_route);
+app.use("/np" ,  np_route);
 
 // if request come from "localhost:3000/pt", then it goes to pediatrician_route
-app.use("/client",cl_route);
-
-//
-app.use("/chef", chef_route);
+app.use("/client" ,  cl_route);
 
 // use errorhandler middleware to handle errors
-app.use(errorHandler)
+app.use( errorHandler )
 
 // this is the prot that server is running on
-const PORT = 3000;
+const PORT = 3001;
 
 // finally start the server on 3000 prot
 app.listen(PORT, () => {
